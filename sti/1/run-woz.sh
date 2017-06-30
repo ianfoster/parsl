7b44c8363c41c86fce2679a9731cbe63e4fee0d1
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-06-30 13:48:55 -0500

    Complete test for Wozniak/Midway

--- sti/1/run-woz.sh
+++ sti/1/run-woz.sh
@@ -4,7 +4,24 @@ set -eu
 # RUN-WOZ
 # This is just a convenience script for Wozniak
 
-PATH=$HOME/sfw/swift-t/stc/bin:$HOME/sfw/Python-2.7.10/bin:$PATH
-export PYTHONPATH=$HOME/sfw/Python-2.7.10/lib/python2.7:$PWD
+# PATH=$HOME/sfw/swift-t/stc/bin:$HOME/sfw/Python-2.7.10/bin:$PATH
+# export PYTHONPATH=$HOME/sfw/Python-2.7.10/lib/python2.7:$PWD
 
-swift-t -l -n 5 workflow.swift
+# Midway:
+SFW=$HOME/Public/sfw
+PY=$SFW/Python-3.6.1
+SWIFT_T=$SFW/login/gcc/swift-t-py-3.6.1
+PATH=$PY/bin:$PATH
+PATH=$SWIFT_T/stc/bin:$PATH
+export PYTHONPATH=$PY/lib/python3.6/site-packages
+
+source ./setup_env.sh
+
+which swift-t python3
+
+swift-t -l -n 6 workflow.swift &
+sleep 5
+python3 test.py
+
+wait
+
